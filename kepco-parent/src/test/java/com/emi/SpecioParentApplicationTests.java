618b50ee1599f5c27a0085378f22d268c8393b4a
윤경희 <kh.yoon@nuritelecom.com>
2020-02-05 14:10:48 +0900

    OPF-1553 - kepco-parent

---
+++ kepco-parent/src/test/java/com/emi/SpecioParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nuri.emi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpecioParentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
