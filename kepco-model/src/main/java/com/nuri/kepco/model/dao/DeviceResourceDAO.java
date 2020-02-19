52385d7898f551de6699d9c92f76e94aa1748641
윤경희 <kh.yoon@nuritelecom.com>
2020-02-19 12:51:05 +0900

    OPF-1612 object

--- kepco-model/src/main/java/com/nuri/kepco/model/dao/DeviceResourceDAO.java
+++ kepco-model/src/main/java/com/nuri/kepco/model/dao/DeviceResourceDAO.java
@@ -1,10 +1,16 @@
 package com.nuri.kepco.model.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 import com.nuri.kepco.model.DeviceResource;
 
 @Mapper
 public interface DeviceResourceDAO extends GenericDAO<DeviceResource> {
+
+	int insertAll(List<DeviceResource> param);
+
+	int delete(DeviceResource param);
 
 }
