8aeadb151f4b8f6f7482359dd52590a6d625d047
guyrune <guyrune@nuritelecom.com>
2020-02-24 13:01:35 +0900

    OPF-1554

--- kepco-fep/src/main/java/com/nuri/kepco/fep/kafka/KafkaReceiver.java
+++ kepco-fep/src/main/java/com/nuri/kepco/fep/kafka/KafkaReceiver.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.nuri.kepco.fep.kafka;
 
 import org.slf4j.Logger;
@@ -48,55 +47,4 @@ public class KafkaReceiver {
     public void notifyDatalisten(@Payload String message) {    	
     	mdDataProcess.process(message);
     }
-=======
-package com.nuri.kepco.fep.kafka;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.stereotype.Service;
-
-import com.nuri.kepco.fep.process.MDDataProcess;
-
-@Service
-public class KafkaReceiver {
-
-	private static final Logger LOG = LoggerFactory.getLogger(KafkaReceiver.class);
-	
-	@Autowired
-	MDDataProcess mdDataProcess;
-	
-	// command 요청에 대한 응답 처리
-    @KafkaListener(topics = "${kafka.topic.mddata}")
-    public void listen(@Payload String message) {    	
-        LOG.info("received message='{}'", message);
-    }
-    
-    @KafkaListener(topics = "${kafka.topic.dpdata}")
-    public void dpDataListen(@Payload String message) {    	
-    	mdDataProcess.process(message);
-    }
-//    
-//    @KafkaListener(topics = "${kafka.topic.eventdata}")
-//    public void eventDatalisten(@Payload String message) {    	
-//        LOG.info("received message='{}'", message);
-//    }
-//    
-//    @KafkaListener(topics = "${kafka.topic.lwm2mdata}")
-//    public void lwm2mDatalisten(@Payload String message) {    	
-//        LOG.info("received message='{}'", message);
-//    }
-//    
-    @KafkaListener(topics = "${kafka.topic.objectlinkdata}")
-    public void objectlinkDatalisten(@Payload String message) {    	
-        mdDataProcess.process(message);
-    }
-    
-    @KafkaListener(topics = "${kafka.topic.notifydata}")
-    public void notifyDatalisten(@Payload String message) {    	
-    	mdDataProcess.process(message);
-    }
->>>>>>> branch 'develop' of https://guyrune@git-n.nuritelecom.com/scm/aimirpf/kepco-lwm2m-5.0.git
 }
--- kepco-fep/src/main/java/com/nuri/kepco/fep/mddata/DataParser.java
+++ kepco-fep/src/main/java/com/nuri/kepco/fep/mddata/DataParser.java
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 package com.nuri.kepco.fep.mddata;
 
 public abstract class DataParser {
 	
 	public abstract void parser(String data, String deviceId, String modemTime) throws Exception;
 	
-=======
-package com.nuri.kepco.fep.mddata;
-
-public abstract class DataParser {
-	
-	public abstract void parser(String data) throws Exception;
-	
->>>>>>> branch 'develop' of https://guyrune@git-n.nuritelecom.com/scm/aimirpf/kepco-lwm2m-5.0.git
 }
