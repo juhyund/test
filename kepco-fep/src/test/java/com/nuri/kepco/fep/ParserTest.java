65a6c1551e0979b93d6442a75d081114e28984e1
윤경희 <kh.yoon@nuritelecom.com>
2020-02-27 15:54:14 +0900

    OPF-1706

--- kepco-fep/src/test/java/com/nuri/kepco/fep/ParserTest.java
+++ kepco-fep/src/test/java/com/nuri/kepco/fep/ParserTest.java
@@ -27,61 +27,11 @@ public class ParserTest {
 
 	@Test
     public void test() {
-		String json = "{\"deviceId\":\"LG-PC02\",\"method\":\"DATAPUSH\",\"format\":\"JSON\",\"token\":\"806662840758920E\",\"mid\":\"18966\",\"payload\":\"{\\\"bn\\\":\\\"/31008/0\\\",\\\"e\\\":[{\\\"n\\\":\\\"101\\\",\\\"sv\\\":\\\"MTIzNDU2NzgxNA\\u003d\\u003d\\\"},{\\\"n\\\":\\\"102/0\\\",\\\"sv\\\":\\\"MjUxOTAwODQ3MzUgARcFFAZQAQA\\u003d\\\"},{\\\"n\\\":\\\"103/0\\\",\\\"sv\\\":\\\"AQBjAQD/AAcCAD0CCgYAAKWdBgAAAAAGAAC9tAYAAZGvCQwH4wwBBwMeAP+AAAEEGBwABAYAAAAABgAAAAAGAAAAAAYAAAAA\\\"}]}\"}";
 		
-		Gson gson = new Gson();
-		
-		ResultMessage resultMessage = new ResultMessage();
-		resultMessage = gson.fromJson(json, ResultMessage.class);
-		
-		String payload = resultMessage.getPayload();
-		String format = resultMessage.getFormat();
-		String method = resultMessage.getMethod();
-		
-	   	Map<Integer, byte[]> meterInfo = new HashMap<Integer, byte[]>();
-	   	Map<Integer, byte[]> meterData = new HashMap<Integer, byte[]>();
-		
-		try {
-		
-			JsonRootObject jsonObject = LwM2mJson.fromJsonLwM2m(payload);
-			String bn = jsonObject.getBaseName();
-			
-			for (JsonArrayEntry e : jsonObject.getResourceList()) {
-				String strPath = bn + "/" + e.getName();
-				
-				LwM2mPath path = new LwM2mPath(strPath);
-				
-				if(path.getObjectId() == 31008) {
-					
-					if(path.getResourceId() == 101) { // modem info
-						
-					}
-					
-					if(path.getResourceId() == 102) { // meter info						
-						if(path.getResourceInstanceId() != null) {
-							meterInfo.put(path.getResourceInstanceId(), Base64.getDecoder().decode(e.getStringValue()));
-						}						
-					}
-					
-					if(path.getResourceId() == 103) { // DLMS_Metering Data 
-						if(path.getResourceInstanceId() != null) {
-							meterData.put(path.getResourceInstanceId(), Base64.getDecoder().decode(e.getStringValue()));
-						}
-					}
-				}			
-			}
-//			MDDataParser parser = new MDDataParser();
-//			
-//			for(Integer key : meterInfo.keySet()) {
-//				meterInfo.get(key);
-//				meterData.get(key);
-//				parser.parseMeterInfo(meterInfo.get(key));
-//				parser.parser(meterData.get(key));
-//			}
+		String json = "{\"deviceId\":\"LG-PC02\",\"method\":\"DATAPUSH\",\"format\":\"JSON\",\"token\":\"806662840758920E\",\"mid\":\"18966\",\"payload\":\"{\\\"bn\\\":\\\"/31008/0\\\",\\\"e\\\":[{\\\"n\\\":\\\"101\\\",\\\"sv\\\":\\\"MTIzNDU2Nzgx\\\"},{\\\"n\\\":\\\"102/0\\\",\\\"sv\\\":\\\"MjQxOTAyMDQyNDIH4woSBRMCAQA=\\\"},{\\\"n\\\":\\\"103/0\\\",\\\"sv\\\":\\\"AAABAAD/AAgCAA4JDAfjChIFEwItAYAAAAEAAAECHgABAgAOCQwH4wETBg4NMgCAAAAAAGIBAR4ABwIAfQYAAGY7BgABGbcGAAAAAAYAAJciFz8M1RIGAAA2NgYAAJcEBgAAAAAGAABQURc/DpD3BgAAMAUGAACCswYAAAAABgAARtEXPwuoxgYAAAAABgAAAAAGAAAAAAYAAAAAF7+AAAAGAAAAAAYAAAAABgAAAAAGAAAAABe/gAAA\\\"}]}\"}";		
+		process.process(json);
+		process.process(json);
 	
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
     }
 	
 }
