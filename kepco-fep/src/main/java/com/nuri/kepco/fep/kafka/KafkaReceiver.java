583ece9279e118b5b053dde52fa8aaa71d0874b3
윤경희 <kh.yoon@nuritelecom.com>
2020-03-04 11:26:19 +0900

    OPF-1553

--- kepco-fep/src/main/java/com/nuri/kepco/fep/kafka/KafkaReceiver.java
+++ kepco-fep/src/main/java/com/nuri/kepco/fep/kafka/KafkaReceiver.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
+import com.nuri.kepco.fep.process.CmdResultDataProcess;
 import com.nuri.kepco.fep.process.MDDataProcess;
 
 @Service
@@ -17,16 +18,20 @@ public class KafkaReceiver {
 	@Autowired
 	MDDataProcess mdDataProcess;
 	
+	@Autowired
+	CmdResultDataProcess cmdResultProcess;
+	
 	// command 요청에 대한 응답 처리
     @KafkaListener(topics = "${kafka.topic.mddata}")
     public void listen(@Payload String message) {    	
-        LOG.info("received message='{}'", message);
+    	cmdResultProcess.process(message);
+    	mdDataProcess.process(message);
     }
-//    
-//    @KafkaListener(topics = "${kafka.topic.dpdata}")
-//    public void dpDataListen(@Payload String message) {    	
-//    	mdDataProcess.process(message);
-//    }
+    
+    @KafkaListener(topics = "${kafka.topic.dpdata}")
+    public void dpDataListen(@Payload String message) {    	
+    	mdDataProcess.process(message);
+    }
     
 //    @KafkaListener(topics = "${kafka.topic.eventdata}")
 //    public void eventDatalisten(@Payload String message) {   
@@ -34,13 +39,13 @@ public class KafkaReceiver {
 ////    	mdDataProcess.process(message);
 //    }
     
-//    @KafkaListener(topics = "${kafka.topic.objectlinkdata}")
-//    public void objectlinkDatalisten(@Payload String message) {    	
-//        mdDataProcess.process(message);
-//    }
-//    
-//    @KafkaListener(topics = "${kafka.topic.notifydata}")
-//    public void notifyDatalisten(@Payload String message) {    	
-//    	mdDataProcess.process(message);
-//    }
+    @KafkaListener(topics = "${kafka.topic.objectlinkdata}")
+    public void objectlinkDatalisten(@Payload String message) {    	
+        mdDataProcess.process(message);
+    }
+    
+    @KafkaListener(topics = "${kafka.topic.notifydata}")
+    public void notifyDatalisten(@Payload String message) {    	
+    	mdDataProcess.process(message);
+    }
 }
